a4e2f7aa4290634d3e7210227c6382c83ece2467
Lewis <lewgoesb00m@gmail.com>
2025-02-21 15:05:21 +0000

    Added Streamlit UI. Artefact now .py over .ipynb. Added some early notes before Week 5 starts.

---
+++ Artifact/! Streamlit Frontend.py
@@ -0,0 +1,83 @@
+# Streamlit provides the frontend UI for the chatbot.
+import streamlit as st
+
+# Import the directed graph (using LangGraph) from the chatbot script.
+from Chatbot import graph
+
+# Used to show if a message was written by the AI or the user,
+from langchain_core.messages import AIMessage, HumanMessage
+
+# ! Comments that start with "noqa" are used in my IDE to stop line length enforcement
+# ! by the Ruff linter.
+
+# Use Streamlit's wide layout, which will use the whole screen space rather than a small
+# centre column. Also gives the page a title and icon which is shown in the 
+# browser tab view.
+st.set_page_config(layout='wide', page_title='University Artifically Intelligent Chatbot',  # noqa: E501
+                   page_icon='📚')
+
+# When the UI first opens, this is the first message that will already be in the chat.
+defaultMsg = "Hello! I'm an assistant chatbot designed to help answer any questions you have about BCU." # noqa: E501
+if 'message_history' not in st.session_state:
+    st.session_state.message_history = [AIMessage(content=defaultMsg)]
+    
+# Organises the app so that the left column (which holds a debug button) is smaller 
+# than the main chat UI.
+left_col, main_col, = st.columns([1, 9])
+
+# In the left column, a button is placed that clears the chat history when clicked.
+# It resets the entire conversation back to the original "Hello!" prompt.
+with left_col:
+    if st.button('[DEBUG] Clear history'):
+        st.session_state.message_history = [AIMessage(content=defaultMsg)]
+
+
+# The main UI is in this column.
+# The user inputs their prompt into a text box which is then sent off to the chatbot.
+with main_col:
+    user_input = st.chat_input("Ask anything about BCU!")
+
+    # If the new message in the chat came from the user, show it as a HumanMessage.
+    if user_input:
+        st.session_state.message_history.append(HumanMessage(content=user_input))
+
+        # Prompts the LLM and RAG tool with the current conversation history.
+        response = graph.invoke({
+            'messages': st.session_state.message_history
+        })
+
+        # The response that gets returned still contains the whole history,
+        # but also appends the latest response. Therefore, make that the new
+        # message history.
+        st.session_state.message_history = response['messages']
+
+    # Shows the running conversation history after any message is sent.
+    # Iterates over the whole message history, showing HumanMessages for the user,
+    # and AIMessages for the LLM.
+    
+    for i in range(1, len(st.session_state.message_history) + 1):
+        this_message = st.session_state.message_history[-i]
+        
+        # ? If the message was written by the LLM:
+        if isinstance(this_message, AIMessage):
+            # ! The LLM returns a blank message following the tool call.
+            # ! I'm not entirely sure why this is, but it can be hidden from the UI.
+            if this_message.content != "":
+                # ? Use a robot profile picture. 
+                message_box = st.chat_message('assistant')
+                message_box.markdown(this_message.content)
+                
+        # ? Alternatively, if the user wrote it:
+        elif isinstance(this_message, HumanMessage):
+            # ? Use a person profile picture.
+            message_box = st.chat_message('user')
+            message_box.markdown(this_message.content)
+        
+    # ? If it isn't an AIMessage or HumanMessage, it must be a ToolMessage.
+    # ? ToolMessages are created when the LLM gets context from the vector DB.
+    # ? It still gets added to the message history, but there's no reason to output it.
+    
+# ! Seems that this is a lot more expensive (literally) than the Jupyter notebook despite minimal changes to the backend.  # noqa: E501
+# TODO: It could be because either context or convo history is being added more than once per message. This MUST be fixed if true.  # noqa: E501
+# TODO: Responses can be a bit shaky at times. It's probably to do with the text splitter chunk sizes?  # noqa: E501
+# TODO: Add old comments and update existing from Jupyter notebook to Chatbot.py. Many were written in Markdown so they got lost when cell contents were cut.  # noqa: E501
---
+++ Artifact/Chatbot.py
@@ -0,0 +1,170 @@
+# Used to get the OpenAI API key from the system environment variables.
+import os
+
+# Initialises the LLM.
+from langchain.chat_models import init_chat_model
+
+# The vector DB used for this prototype. It's worked well this far,
+# so I'll likely use it in future versions, too.
+from langchain_community.vectorstores import FAISS
+
+# Allows for inputs to be sent to the LLM as the 
+# system (specialised prompt that defines LLM behaviour).
+from langchain_core.messages import SystemMessage
+
+# Allows for "tools" to be created. The LLM can use these tools
+# to execute defined code, such as retrieving data from the FAISS DB.
+from langchain_core.tools import tool
+
+# Used to embed queries to the FAISS DB.
+from langchain_openai import OpenAIEmbeddings
+
+# LangGraph key functionality. Declares a clear and consistent 
+# structure for the chatbot. LangGraph is described in detail in 
+# its own notebook section.
+from langgraph.graph import END, MessagesState, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+
+# Get the OpenAI API key from environment variables so that 
+# it's not visible in this code on GitHub. OpenAI would revoke the key if it leaked.
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+# Sets the directory of the FAISS DB that's being loaded from.
+    # Options:
+    #   FAISS: Chunk size 1000, Overlap 200, UnstructuredPDFLoader in elements mode.
+    #   FAISS-PyPDF: Chunk size 1000, Overlap 200, PyPDFLoader with default args.
+FAISS_PATH = "FAISS-PyPDF"
+# Experimentation showed that using the PyPDFLoader-based DB gave better results.
+
+# Sets up the embedding model with the API key.
+embedder = OpenAIEmbeddings(
+    model = "text-embedding-3-small",
+    api_key = os.environ["OPENAI_API_KEY"]
+)
+
+# Load the vector database.
+db = FAISS.load_local(folder_path = FAISS_PATH,
+                      embeddings = embedder,
+                      allow_dangerous_deserialization=True) 
+
+
+# Initialise the model.
+llm = init_chat_model("gpt-4o-mini", temperature = 0)
+
+
+@tool(response_format = "content_and_artifact")
+def retrieve(query):
+    # The docstring below is actually REQUIRED by LangGraph, and this 
+    # won't run without it.
+    """Retrieves the 3 most relevant chunks for the user's query."""
+    retrieved_docs = db.similarity_search(query, k = 3)
+    
+    # The chunk's content and the document it came from (e.g "Attendance.pdf")
+    content = "\n\n".join(
+        (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
+        for doc in retrieved_docs
+    )
+    return content, retrieved_docs
+
+
+# Initialise an empty graph. Nodes and edges are added later.
+graph_builder = StateGraph(MessagesState)
+
+def query_or_respond(state: MessagesState):
+    # Allows the LLM to use the retrieve tool that was created earlier.
+    rag_llm = llm.bind_tools([retrieve])
+    
+    # The LLM decides on its own if it needs
+    response = rag_llm.invoke(state["messages"])
+    
+    # A key element of using a MessagesState is that that will append
+    # the response to the conversation history rather than overwriting.
+    return {"messages": [response]}
+
+
+# Creating a node for the list of tools that the LLM can access. 
+# At the moment, it's only one tool, the retriever, but other tools can 
+# easily be added later by appending them to this list.
+tools = ToolNode([retrieve])
+
+# The final step of the process, generating a message based on the info gathered
+# from the retrieval tool.
+def generate(state: MessagesState):
+    # To massively reduce token consumption (and therefore cost),
+    # the most recent RAG context from tool calls is added to the 
+    # prompt to stop the LLM searching the entire conversation history 
+    # for something that was JUST said. 
+    recent_tool_messages = []
+    
+    # To get the most recent ones, the list needs to be reversed
+    # so that the most recent come first instead of last.
+    for message in reversed(state["messages"]):
+        if message.type == "tool":
+            recent_tool_messages.append(message)
+        else:
+            # If it's a normal message, stop.
+            break
+    
+    # Put the tool messages in their original order in case 
+    # the sequence of the retrieved context mattered. 
+    tool_messages = recent_tool_messages[::-1]
+
+    # Saves the context from the recent tool messages.
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    
+    # This is the LLM's system prompt, which decides how the LLM behaves.
+    system_message_content = (
+        # This is formatted like this to follow the Ruff linter's line length rule.
+        "You are an assistant to help new students get acclimated to Birmingham City "
+        "University. Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. When referring to context, be specific and quote the context. "
+        "Use five sentences maximum and keep the answer concise."
+        "\n\n"
+        f"Context: {docs_content}" # RAG context is attached here
+    )
+    
+    # The list of messages in the conversation.
+    # Only adds messages that AREN'T tool calls, as tool calls
+    # would hugely increase the input tokens used, and the LLM 
+    # should (hopefully) have already said the useful info in its response
+    # so it can use that instead.
+    conversation_messages = [
+        message for message in state["messages"] # Every message in the conversation
+        if message.type in ("human", "system")# If it's human input or the system prompt
+        or (message.type == "ai" and not message.tool_calls)# Or AI and not a tool call.
+    ]
+    
+    # The final prompt consists of the system prompt and the conversation.
+    # This does mean that as a conversation continues, token cost will greatly increase.
+    prompt = [SystemMessage(system_message_content)] + conversation_messages
+
+    # Get the LLM's response to the prompt and return the response.
+    response = llm.invoke(prompt)
+    return {"messages": [response]}
+
+
+# Add all the nodes.
+graph_builder.add_node(query_or_respond)
+graph_builder.add_node(tools)
+graph_builder.add_node(generate)
+
+
+graph_builder.set_entry_point("query_or_respond")
+graph_builder.add_conditional_edges(
+    "query_or_respond",
+    tools_condition, 
+    # If retrieval is not needed, generate a general answer.
+    # If it is, call the retrieval tool.
+    {END: END, "tools": "tools"},
+)
+
+# An edge is also needed between the tool call and response generation
+# to ensure the response has the RAG context.
+graph_builder.add_edge("tools", "generate")
+
+# After the response is generated, the graph is done.
+graph_builder.add_edge("generate", END)
+
+# Compile the graph
+graph = graph_builder.compile()
